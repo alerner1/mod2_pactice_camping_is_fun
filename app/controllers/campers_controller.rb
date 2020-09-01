f55d2226352a49b382bc68d3be003edcda7a7963
alerner1 <alyssplerner@gmail.com>
2020-09-01 11:35:14 -0400

    finished and tested all deliverables

--- app/controllers/campers_controller.rb
+++ app/controllers/campers_controller.rb
@@ -1,5 +1,34 @@
 class CampersController < ApplicationController
+  before_action :find_camper, only: :show
+
   def index
     @campers = Camper.all
+  end
+
+  def show
+  end
+
+  def new
+    @camper = Camper.new
+  end
+
+  def create
+    @camper = Camper.new(camper_params)
+    if @camper.valid?
+      @camper.save
+      redirect_to camper_path(@camper)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def find_camper
+    @camper = Camper.find(params[:id])
+  end
+
+  def camper_params
+    params.require(:camper).permit(:name, :age)
   end
 end
